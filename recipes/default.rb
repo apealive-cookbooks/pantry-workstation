fec67ca58b5e69b8661910e2026cc150b389b644
Petr Michalec <epcim@apealive.net>
2016-06-13 08:23:08 +0200

    exec homeshick, extr pkgs only on supp. platforms

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,16 @@
 
 include_recipe "pantry::default"
+
 include_recipe "#{cookbook_name}::users"
-include_recipe "#{cookbook_name}::homeshick"
 include_recipe "#{cookbook_name}::lang-packages"
-include_recipe "#{cookbook_name}::extr-packages"
+
+case node['platform_family']
+when 'debian', 'rhel'
+  include_recipe "#{cookbook_name}::homeshick"
+  include_recipe "#{cookbook_name}::extr-packages"
+when 'mac_os_x'
+  include_recipe "#{cookbook_name}::homeshick"
+  # TODO osx handler
+when 'windows'
+  # TODO windows handler
+end
